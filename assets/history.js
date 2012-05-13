212f099646953f3340472ed4dd3940bc37aad4c6
Martin Reurings <martin@windgazer.nl>
2012-05-13 15:09:23 +0200

    Improved readability by adding comments :)

--- assets/history.js
+++ assets/history.js
@@ -7,14 +7,27 @@
  */
 (function(){
 
+	/**
+	 * Storage and retreival of historical data
+	 */
 	var History = {
 
+		/**
+		 * Store a URL in whatever permanent storage is defined.
+		 */
 		storeURL: function( id, url ) {
 
 			return Settings.setUrl( id, url );
 
 		},
-		
+
+		/**
+		 * Get a url from whatever permanent storage is defined.
+		 * Probably will start using a caching mechanism to be able to do real-time retreival
+		 * and storage, while allowing asynchronous syncing in the background without 'hurting'
+		 * the user.
+		 * 
+		 */
 		retreiveURL: function( id ) {
 
 			return Settings.getUrl( id );
@@ -23,14 +36,15 @@
 
 	}
 
+	//Capture the comic-received event and store it in history. By using this pubsub system the rest of the code
+	//does not need to know how history works, only that it's there to get the history from...
 	ce.attachEvent( nl.windgazer.COMIC_EVENT_ID, function( eid, data ) {
-		
-		console.log("Storing new entry", data.comic.id, data.URL);
+
+		if ( Options && Options.isDebug() ) console.log("Storing new entry", data.comic.id, data.URL);
 
 		History.storeURL( data.comic.id, data.URL );
 
 	} );
-
 
 	window.History = History;
 
