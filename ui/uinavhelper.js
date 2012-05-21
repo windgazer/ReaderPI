e7bce64b918329e58fc89a22737b799fa80800eb
Martin Reurings <martin@windgazer.nl>
2012-05-21 16:19:34 +0200

    Touch-pad swipe support improved for drop down top-menu and created for 'previous' / 'next' navigation. Now that I'm feeling confident about the scroll-support on desktop, I will want to improve this by adding the same style support for touch-os devices.

--- ui/uinavhelper.js
+++ ui/uinavhelper.js
@@ -48,24 +48,86 @@
 
 	LinkListener.addHandler( "latestEntry", fetchLatest );
 
+	var scrollTracker = { x: 0, y: 0, vert: true, velocity: 0, t: 0 };
+
 	function screenEdgeEventHandler( e ) {
 		e = e ? e : window.event;
 		var wheelData = e.detail ? e.detail * -1 : e.wheelDelta / 30;
-		var body = document.getElementsByTagName("body")[0];
+		var isVertical = false;
+		var isEdgeX = scrollTracker.x === window.scrollX;
+		var atEdgeLeft = window.scrollX <= 0;
+		var atEdgeRight = document.body.clientWidth + window.scrollX >= document.body.scrollWidth;
+		var isEdgeY = scrollTracker.y === window.scrollY;
+		var atEdgeTop = window.scrollY <= 0; //Small grace-area (due to 'bounce' in some browser UI's)
+		var atEdgeBottom = document.body.clientHeight + window.scrollY >= document.body.scrollHeight;
+		var now = new Date().getTime();
+		var timeout = now - scrollTracker.t;
 
-		if ( wheelData > 0 && window.scrollY <= 0 ) {
-			if (wheelData > 6) {
+		//Some hackeridoo to figure this out between WebKit / Mozilla
+		if ( typeof e.axis === "undefined" ) {
+			isVertical = Math.abs( e.wheelDeltaY ) > Math.abs( e.wheelDeltaX );
+		} else {
+			isVertical = e.axis > 1;
+		}
+		var body = document.getElementsByTagName("body")[0];
+		
+		//console.debug("Checking outcome (wheelData, isVertical, isEdgeX, isEdgeY, e)", wheelData, isVertical, isEdgeX, isEdgeY, e);
+
+		if ( isVertical ) {
+
+			if ( atEdgeTop && ( timeout > 1000 ) && wheelData > 3 ) {
+
 				if ( Options.isDebug() ) console.debug( "Show top menu" );
 				body.className += " menuEnabled";
+
+			} else if ( wheelData < 0 ) {
+
+				if ( Options.isDebug() ) console.debug( "Hide top menu" );
+				body.className = body.className.replace(/ ?\bmenuEnabled\b/g, "");
+
+			} else {
+
+				//Reset timeout to previous state if at the edge...
+				if ( atEdgeTop || atEdgeBottom ) {
+
+					now = scrollTracker.t;
+
+				}
+
 			}
-		} else if ( wheelData < 0 ) {
-			if ( Options.isDebug() ) console.debug( "Hide top menu" );
-			body.className = body.className.replace(/ ?\bmenuEnabled\b/g, "");
+
+		} else {
+			
+			//console.log(( timeout > 100 ), wheelData, isEdgeX);
+
+			if ( atEdgeLeft && ( timeout > 150 ) && wheelData > 0 ) {
+
+				console.log("PREV SWIPE", e);
+				fetchPrevious();
+
+			}
+
+			if ( atEdgeRight && ( timeout > 150 ) && wheelData < 0 ) {
+
+				console.log("NEXT SWIPE", e);
+				fetchNext();
+
+			}
+
 		}
+
+		scrollTracker = { x: window.scrollX, y: window.scrollY, vert: isVertical, velocity: wheelData, t: now };
+
 	}
 
 	Events.attach( window, "mousewheel", screenEdgeEventHandler );
 	Events.attach( window, "DOMMouseScroll", screenEdgeEventHandler );
+
+	ce.attachEvent( Constants.COMIC_FETCH_INPROGRESS, function(){
+
+		document.body.className = document.body.className.replace(/ ?\bmenuEnabled\b/g, "");
+
+	});
 
 	function selectNewComic() {
 		var cs = document.getElementById("comicSelector");
